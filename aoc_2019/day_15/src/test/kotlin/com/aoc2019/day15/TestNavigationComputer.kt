0d0aa4665e603373c76dfac5357576b900cc5735
Rick <richardgale90@gmail.com>
2020-02-25 00:36:51 +0000

    Added basic tests for day 15 part 1

--- aoc_2019/day_15/src/test/kotlin/com/aoc2019/day15/TestNavigationComputer.kt
+++ aoc_2019/day_15/src/test/kotlin/com/aoc2019/day15/TestNavigationComputer.kt
@@ -1,9 +1,25 @@
 package com.aoc2019.day15
 
 import com.aoc2019.common.computer.Computer
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.greaterThan
+import org.junit.Test
 
 class TestNavigationComputer {
 
     val computer = NavigationComputer(Computer.from(javaClass.getResource("/input.txt").readText()))
+
+    @Test
+    fun setup() {
+        assertThat(computer.knownPositions.size, `is`(5))
+    }
+
+    @Test
+    fun explore() {
+        computer.explore()
+
+        assertThat(computer.knownPositions.size, greaterThan(10))
+    }
 
 }
